2d62e55f88ad3f01e6d5b464b79da8396a10b499
riadan710 <riadan710@gmail.com>
2025-04-07 14:38:15 +0900

    5주차 실습

--- calculate.py
+++ calculate.py
@@ -1,4 +1,5 @@
 ### 사칙연산 함수 정의
+
 def plus(a,b):
     return int(a) + int(b)
 
@@ -15,22 +16,30 @@ if __name__ == '__main__':
 
     ### 사용자 입력
     print('\n첫번째 숫자를 입력하세요.')
-    input1 = input('입력: ')
+    num1 = input('입력: ')
 
     print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
-    act = input('기호: ')
+    operator = input('기호: ')
 
     print('\n두번째 숫자를 입력하세요.')
-    input2 = input('입력: ')
+    num2 = input('입력: ')
 
 
     ### 연산 수행
-    if act == '+':
-        result = plus(input1, input2)
-    elif act == '-':
-        result = minus(input1, input2)
-    elif act == '*':
-        result = mul(input1, input2)
-    elif act == '/':
-        result = divide(input1, input2)
-    print(f'사칙연산 결과는 {result}입니다.')
+    if operator == '+':
+        result = plus(num1, num2)
+    elif operator == '-':
+        result = minus(num1, num2)
+    elif operator == '*':
+        result = mul(num1, num2)
+    elif operator == '/':
+        result = divide(num1, num2)
+    print('사칙연산 결과는 {result}입니다.')
+    ##흠 주석 잘 들어감?
+    ### 시진핑 주석.
+    ### 시진핑핑이<-write By 권.
+    ## 난 그사람 얼굴도 몰라
+    ### 진짜에요?
+    #### 증거를 다 남겨야돼
+    ##흠 이제야 산다 했군
+    
